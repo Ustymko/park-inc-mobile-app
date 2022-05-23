4b25c4341c8d6339754e57b40d2b1ae40189d0ef
Yurasj <yura.rozhankivskyy@gmail.com>
2022-05-23 16:23:43 +0300

    History in account screen

--- components/Account/style.js
+++ components/Account/style.js
@@ -41,11 +41,46 @@ const styles = StyleSheet.create({
     },
     
     historyParkingPlace:{
+        flex: 1,
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
         height: 100,
         width: 400,
-        backgroundColor: '#fff',
+        backgroundColor: '#5e89f7',
         marginBottom: 10
-    }
+    },
+    buttonsText: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: '100',
+        textAlign: 'left',
+        paddingLeft: 5,
+        paddingTop: 30,
+      },
+      parkingPlaceText: {
+        color: 'white',
+        fontSize: 25,
+        fontWeight: '100',
+        textAlign: 'left',
+        paddingLeft: 5,
+      },
+      parkingPlaceHours: {
+        color: 'white',
+        fontSize: 25,
+        fontWeight: '100',
+        textAlign: 'center',
+        marginLeft: 150,
+        bottom: 90,
+        
+      },
+      parkingPlaceDate: {
+        color: 'white',
+        fontSize: 25,
+        fontWeight: '100',
+        textAlign: 'center',
+        marginLeft: 150,
+        bottom: 70,
+      }
 
 });
 
