8a67175495df9884f5eca28dd59badbdaa735bde
Ustym Buchko <ustym.buchko1@gmail.com>
2022-06-09 13:26:09 +0300

    style of my places screen

--- screens/MyPlacesScreen/style.js
+++ screens/MyPlacesScreen/style.js
@@ -1,21 +1,43 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    panel:{
-        backgroundColor: '#5e89f7',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '7%',
-        width: '100%',
-        flexDirection: 'row',
-        fontSize: 10,
-    },
-    panelText:{
-        fontSize: 40,
-        color: 'white',
-
-
-    },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        marginTop: 48,
+        width: '100%'
+      },
+      place:{
+          backgroundColor: '#adffa8',
+          margin: 10,
+          alignItems: 'center'
+      },
+      movieText: {
+        fontSize: 26,
+        fontWeight: "200",
+      },
+      openTheGates:{
+          borderWidth: 3,
+      },
+      topPanel:{
+          backgroundColor: '#0893cf',
+          alignItems: 'center',
+          height: '7%'
+        }, 
+        topPanelText:{
+          flex: 5,
+          color: '#ffffff',
+          fontSize: 35,
+          paddingLeft: '15%'
+        },
+        backButton:{
+          flex: 1,
+          marginLeft: '3%'
+        }, 
+        backButtonArrow:{
+          color: '#ffffff',
+          fontSize: 35
+        },
 })
 
 export default styles
