1eade4f34821324e5ce45685f3635108dc67d28f
Ustym Buchko <ustym.buchko1@gmail.com>
2022-06-10 21:24:09 +0300

    sending post request when open the gates button is pressed

--- screens/MyPlacesScreen/index.js
+++ screens/MyPlacesScreen/index.js
@@ -6,6 +6,7 @@ import React, {useState, useEffect} from 'react'
 
 var statusBarHeight = Constants.statusBarHeight
 const movieURL = "https://reactnative.dev/movies.json";
+const iftttUrl = "https://ostap1729.pythonanywhere.com/sonoff_ifttt"
 const DATA = [
     { "id": "1", "address": "Vyhovskoho 10", "price": "10" },
     { "id": "2", "address": "Vyhovskoho 12", "price": "20" },
@@ -15,8 +16,18 @@ const MyPlacesScreen = () => {
     const navigation = useNavigation();
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
+    const [isButtonOpenTheGatesDisabled, setIsButtonOpenTheGatesDisabled] = useState(false);
     var backButton = "<--"
-  // similar to 'componentDidMount', gets called once
+    const openTheGatesFunc = () => {
+      setIsButtonOpenTheGatesDisabled(true)
+      fetch(iftttUrl, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ state: 'open' })
+      })
+      .catch((error) => console.error(error))
+      setIsButtonOpenTheGatesDisabled(false)
+    }
   useEffect(() => {
     // fetch(movieURL)
     //   .then((response) => response.json()) // get response, convert to json
@@ -43,7 +54,7 @@ const MyPlacesScreen = () => {
       {isLoading ? (
         <ActivityIndicator />
       ) : (
-        <View style={{width: "100%"}}>
+        <View style={{width: '100%'}}>
           <FlatList
             style={{width: '100%'}}
             data={data}
@@ -57,10 +68,13 @@ const MyPlacesScreen = () => {
                 </View>
                 <View style={{flex: 8, alignItems: 'center'}}>
                   <Text style={styles.address}>{item.address}</Text>
-                  <View style={{height: 1, borderColor: "black", borderBottomWidth: 1, width: '80%'}}></View>
+                  <View style={{height: 1, borderColor: 'black', borderBottomWidth: 1, width: '80%'}}></View>
                   <Text style={styles.price}>{item.price} hrn/hour</Text>
                   <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                    <TouchableOpacity style={styles.openTheGates}>
+                    <TouchableOpacity style={styles.openTheGates} 
+                                      disabled={isButtonOpenTheGatesDisabled}
+                                      onPress={openTheGatesFunc}
+                    >
                       <Text style={styles.openTheGatesText}>Open the gates</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={{flex: 1, alignItems: 'center'}}>
